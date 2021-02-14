cdd003870e9b4864d112a3aa509218a9640955d8
Brush87 <dan.huot87@gmail.com>
2021-02-14 00:58:30 -0700

    Mongoose & Express + Extra changes Mongo Init

--- mongo-init.js
+++ mongo-init.js
@@ -1,16 +1,29 @@
+
 const axios = require('axios').default
+
+const baseNHLApi = 'https://statsapi.web.nhl.com'
+const teamListApi = '/api/v1/teams'
+const seasonStatsApi = '/stats?stats=statsSingleSeason&season=20192020' // TODO Change to appropriate year
 
 const initializeMongoDB = async () => {
   // Get Teams List
   const getTeamsList = async () => {
-    return await axios.get('https://statsapi.web.nhl.com/api/v1/teams').then(res => {
+    return await axios.get(`${baseNHLApi}${teamListApi}`).then(res => {
       return res.data.teams
+    })
+  }
+
+  const getTeamRoster = async (team) => {
+    return await axios.get(`${baseNHLApi}${team.link}/roster`).then(res => {
+      console.log('Roster', res.data.roster)
     })
   }
 
   const teamsList = await getTeamsList()
 
-  console.log('The Teams List', teamsList)
+  console.log('The Teams List', teamsList[0])
+
+  console.log(await getTeamRoster(teamsList[0]))
 }
 
 initializeMongoDB()
