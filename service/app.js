cba2420fe0b13b099cbd3f8a739626c51765f2cf
Brush87 <dan.huot87@gmail.com>
2021-02-14 23:56:40 -0700

    More small changes

--- service/app.js
+++ service/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path')
-const routes = require('../routes/api')
+const routes = require('./routes/api')
 
 const app = express()
 
